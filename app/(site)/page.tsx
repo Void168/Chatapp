583c48b0fb19f19e08a748649ce855027590f20e
LPHung <phuhung16820@gmail.com>
2023-07-20 17:35:02 +0700

    create form for auth page, email field for auth form

---
+++ app/(site)/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import AuthForm from "../components/form/Authform";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col justify-center items-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-lg">
+        <Image
+          alt="logo"
+          height={48}
+          width={48}
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Đăng nhập vào tài khoản của bạn
+              </h2>
+              <AuthForm />
+      </div>
+    </div>
+  );
+}
---
+++ app/components/form/Authform.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import Input from "../input/Input";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+    }
+
+    if (variant === "LOGIN") {
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input id="email" label="Email" register={register} errors={errors}/>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
