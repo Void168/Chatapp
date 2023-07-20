5b4f0fc70f3202a3b210666c3103da8073286bc4
LPHung <phuhung16820@gmail.com>
2023-07-20 17:48:52 +0700

    create button concept for the web

--- app/components/form/Authform.tsx
+++ app/components/form/Authform.tsx
@@ -3,6 +3,7 @@
 import React, { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 import Input from "../input/Input";
+import Button from "@/app/components/Button";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -47,7 +48,26 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input id="email" label="Email" register={register} errors={errors}/>
+          {variant === "REGISTER" && (
+            <Input
+              id="email"
+              label="Email"
+              register={register}
+              errors={errors}
+            />
+          )}
+          <Input id="email" label="Email" register={register} errors={errors} />
+          <Input
+            id="password"
+            label="Mật khẩu"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Đăng nhập" : "Đăng ký"}
+            </Button>
+          </div>
         </form>
       </div>
     </div>
