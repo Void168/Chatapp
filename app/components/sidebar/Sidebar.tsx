695c06e056c594829a5f4835017369da6cfc4a98
LPHung <phuhung16820@gmail.com>
2023-07-23 20:51:56 +0700

    create mobile footer UI

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 export async function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
       <DesktopSidebar />
+      <MobileFooter />
       <main>{children}</main>
     </div>
   );
