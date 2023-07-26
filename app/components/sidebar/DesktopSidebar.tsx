f000b3e686a36b6d212583988fdfb0fd73041edc
LPHung <phuhung16820@gmail.com>
2023-07-27 01:53:54 +0700

    create setting modal UI

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -11,6 +11,7 @@ import Avatar from "../Avatar";
 import { signOut } from "next-auth/react";
 import { HiArrowLeftOnRectangle } from "react-icons/hi2";
 import clsx from "clsx";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -21,37 +22,44 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
-      <nav className="mt-4 flex flex-col justify-between">
-        <ul role="list" className="flex flex-col items-center space-y-1">
-          {routes.map((item) => (
-            <DesktopItem
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-            />
-          ))}
-          <button className="w-full h-full"></button>
-          <li
-            onClick={() => signOut()}
-            className="group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100 cursor-pointer"
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+        <nav className="mt-4 flex flex-col justify-between">
+          <ul role="list" className="flex flex-col items-center space-y-1">
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+              />
+            ))}
+            <button className="w-full h-full"></button>
+            <li
+              onClick={() => signOut()}
+              className="group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100 cursor-pointer"
+            >
+              <HiArrowLeftOnRectangle className="h-6 w-6 shrink-0" />
+              <span className="sr-only">Đăng xuất</span>
+            </li>
+          </ul>
+        </nav>
+        <nav className="mt-4 flex flex-col justify-center items-center">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="cursor-pointer hover:opacity-75 transition"
           >
-            <HiArrowLeftOnRectangle className="h-6 w-6 shrink-0" />
-            <span className="sr-only">Đăng xuất</span>
-          </li>
-        </ul>
-      </nav>
-      <nav className="mt-4 flex flex-col justify-center items-center">
-        <div
-          onClick={() => setIsOpen(true)}
-          className="cursor-pointer hover:opacity-75 transition"
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
